377ee3a42db5a54c25939119a07a1f00781bc03a
Sergey Ryabin <ryabin.ss@gmail.com>
2020-04-28 00:56:21 +0400

    second commit

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+docker-compose build
+docker-compose up
+
+echo 127.0.0.1 g.g | sudo tee -a /etc/hosts
+```
+http://g.g/arg?=foobar
---
+++ files/tmuxinator.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+T_NAME=$1
+
+test -f ${T_CONF_DIR}/${T_NAME}.yml && {
+	tmuxinator start ${T_NAME}
+} || {
+	tmuxinator new ${T_NAME} && tmuxinator start ${T_NAME}
+} 
